bfa40ed32b17f699e58045978a1d6260a6c113bc
Jlius Suweno <jliuss@live.com>
2020-10-12 21:29:37 +0700

    update final

---
+++ ByteCoin/Model/BitcoinData.swift
@@ -0,0 +1,11 @@
+//
+//  BitcoinData.swift
+//  ByteCoin
+//
+//  Created by Jlius Suweno on 05/10/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,39 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "ED2FD7FE-0A57-4098-8D11-D09ED29E2689"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String){
+        let stringURL = "\(baseURL)/\(currency)"
+        print(stringURL)
+        performRequest(with: stringURL)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+//                    self.delegate?.didFailWithError(error)
+//                    return nil
+                }
+//                if let safeData = data {
+//                    if let bitcoinData = self.parseJSON(data) {
+//                        self.delegate?.didUpdatePrice(bitcoinData: bitcoinData)
+//                    }
+//                }
+            }
+            task.resume()
+        }
+    }
 
+//    func parseJSON(_ bitcoinData: BitcoinData) -> BitcoinModel? {
+//        let decoder = JSONDecoder()
+//        do {
+//            let decodedData = try decoder.decode(<#T##type: Decodable.Protocol##Decodable.Protocol#>, from: <#T##Data#>)
+//        }
+//    }
     
 }
